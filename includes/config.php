010d3597edf65d692b68c47ed343abe072a72e54
Kilyavi <kilyavi@gmail.com>
2018-11-18 00:59:54 +0400

    1

---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php
+
+$config = array(
+	'title' => 'BarCraft - Игровое Сообщество',
+	'db' => array(
+		'server' => 'localhost',
+		'username' => 'root',
+		'password' => '',
+		'name' => 'test,
+		')
+	);
--- test.php
+++
@@ -1,5 +0,0 @@
-<?php print $_GET['message']; ?>
-<form action="">
-<input type="text" name="message" />
-<input type="submit" />
-</form>
